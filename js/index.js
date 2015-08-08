7b247d5fb457923020f86bd679fe35aeaf64b8db
David Royer <droyer01@gmail.com>
2015-08-08 18:18:55 -0400

    First commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+(function() {
+    
+    
+    var navToggle = $('.nav-toggle');
+    
+    navToggle.on('click', openNavDrawer);
+    
+    function openNavDrawer(event) {
+        event.preventDefault();	
+		$('.nav').toggleClass('nav--open');
+		$('.main').toggleClass('main--open');
+    }
+    
+})();
