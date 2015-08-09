41fbac86b51ebc01dc2759c22c3c5bd49d9c6f73
David Royer <droyer01@gmail.com>
2015-08-09 13:50:03 -0400

    animated hamburger icon

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
 (function() {
     
     
-    var navToggle = $('.nav-toggle');
+    var navToggle = $('.nav-trigger');
     
     navToggle.on('click', openNavDrawer);
     
     function openNavDrawer(event) {
         event.preventDefault();	
+        $('body').toggleClass('navigation-is-open');
 		$('.nav').toggleClass('nav--open');
 		$('.main').toggleClass('main--open');
     }
